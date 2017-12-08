082e13c9c0a548e42cd7df14f5619b08ce616421
Harun Davood <davoodharun@gmail.com>
2017-12-07 22:06:30 -0500

    merged accountmanagmentprincipals into customScriptExtension

--- custom-scripts/customScriptBaseline.ps1
+++ custom-scripts/customScriptBaseline.ps1
@@ -351,31 +351,6 @@
         )
     } # End of Config Data
 
-try{
-    # Set Password Policy
-
-    if([string]::IsNullOrWhiteSpace($MachinesToSetPasswordPolicy)){
-        $adMachineArray = $MachinesToSetPasswordPolicy.Split(";")
-        $index = $adMachineArray.IndexOf($MachineName)
-        if($index -gt -1){
-
-            $Domain = (gwmi WIN32_ComputerSystem).Domain
-
-            Import-Module ActiveDirectory
-            Import-Module grouppolicy
-            $dcs = $Domain.split(".")
-            $target = "DC=" + $dcs[0]+ "," + "DC=" + $dcs[1]
-
-            #this does not error out
-            Set-ADDefaultDomainPasswordPolicy -Identity $Domain -AuthType Negotiate -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -PasswordHistoryCount 24 -ComplexityEnabled $true -ReversibleEncryptionEnabled $true -MinPasswordLength 14
-            Set-GPLink -Guid (Get-GPO -Name "Default Domain Policy").id -Target $target -LinkEnabled Yes -Enforced Yes
-            .\accountmanagementprincipals.ps1
-        }
-
-    }
-}
-catch{
-}
 
 try {
       if([string]::IsNullOrWhiteSpace($SQLPrimaryName)){
@@ -454,7 +429,62 @@ try{
         $adMachineArray = $MachinesToSetPasswordPolicy.Split(";")
         $index = $adMachineArray.IndexOf($MachineName)
         if($index -gt -1){
-            .\accountmanagementprincipals.ps1
+
+            $Domain = (gwmi WIN32_ComputerSystem).Domain
+
+            Import-Module ActiveDirectory
+            Import-Module grouppolicy
+            $dcs = $Domain.split(".")
+            $target = "DC=" + $dcs[0]+ "," + "DC=" + $dcs[1]
+
+            #this does not error out
+            Set-ADDefaultDomainPasswordPolicy -Identity $Domain -AuthType Negotiate -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -PasswordHistoryCount 24 -ComplexityEnabled $true -ReversibleEncryptionEnabled $true -MinPasswordLength 14
+            Set-GPLink -Guid (Get-GPO -Name "Default Domain Policy").id -Target $target -LinkEnabled Yes -Enforced Yes
+
+            $Domain = (gwmi WIN32_ComputerSystem).Domain
+            $Root = [ADSI]"LDAP://RootDSE"
+            $LDAPDomain = $Root.Get("rootDomainNamingContext")
+            $key = "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop"
+            $key1 = "HKLM\Software\Microsoft\Windows\CurrentVersion\policies\system"
+
+
+            #PART 1: set Account lockout policies
+            Set-ADDefaultDomainPasswordPolicy -Identity $Domain -AuthType Negotiate -LockoutDuration "03:00:00" -LockoutObservationWindow "00:15:00" -LockoutThreshold "3"
+
+
+            #PART 2: set inactive session policies
+            New-GPO 'session lock gpo' |`
+            Set-GPRegistryValue -Key $key -ValueName 'ScreenSaveTimeOut' -Type String -Value 900 |`
+            Set-GPRegistryValue -Key $key -ValueName 'ScreenSaveActive' -Type String -Value 1 |`
+            Set-GPRegistryValue -Key $key -ValueName 'SCRNSAVE.EXE' -Type String -Value "rundll32 user32.dll,LockWorkStation" |`
+            Set-GPPermissions -Replace -PermissionLevel None -TargetName 'Authenticated Users' -TargetType group | `
+            Set-GPPermissions -PermissionLevel gpoapply -TargetType group -TargetName 'Authenticated Users' | New-GPLink -Domain $Domain -Target $LDAPDomain -enforced yes -Order 1
+
+
+            #PART 3: set logon message Policy
+            New-GPO 'logon message gpo' |`
+            Set-GPRegistryValue -Key $key1 -ValueName 'legalnoticecaption' -Type string -Value "Sample system use notification" |`
+            Set-GPRegistryValue -Key $key1 -ValueName 'legalnoticetext' -Type string -Value "Sample system use notification. Customer must edit this text to comply with customer organization and/or regulatory body requirements." |`
+            Set-GPPermissions -Replace -PermissionLevel None -TargetName 'Authenticated Users' -TargetType group | `
+            Set-GPPermissions -PermissionLevel gpoapply -TargetType group -TargetName 'Authenticated Users' | New-GPLink -Domain $Domain -Target $LDAPDomain -enforced yes -Order 3
+
+
+            #PART 4: set logoff message Policy
+            New-GPO 'logoff message gpo' |`
+            Set-GPRegistryValue -Key $key1 -ValueName 'verbosestatus' -Type DWORD -Value 1 |`
+            Set-GPPermissions -Replace -PermissionLevel None -TargetName 'Authenticated Users' -TargetType group | `
+            Set-GPPermissions -PermissionLevel gpoapply -TargetType group -TargetName 'Authenticated Users' | New-GPLink -Domain $Domain -Target $LDAPDomain -enforced yes -Order 2
+
+
+
+            ##################################
+
+
+
+            #########################################################################
+
+            #this is required for policies to apply- should be applied at the end of the Custom script extension only
+            Invoke-GPUpdate -Boot
         }
 
     }
