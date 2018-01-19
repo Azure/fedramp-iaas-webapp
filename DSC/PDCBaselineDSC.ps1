fe70cad7d42483f787dedc5de49e45a7eb456dd9
mik-e-kim <33297027+mik-e-kim@users.noreply.github.com>
2018-01-19 13:53:40 -0800

    Dev Branch Update

---
+++ DSC/PDCBaselineDSC.ps1
@@ -0,0 +1,196 @@
+﻿configuration PDCBaselineDSC
+{
+   param
+   (
+        ### Create AD PDC ###
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30,
+
+        ### Configure Antimalware ###
+        [string]$IsSingleInstance = "Yes",
+		[bool]$CheckForSignaturesBeforeRunningScan,
+		[bool]$DisableArchiveScanning,
+		[bool]$DisableAutoExclusions,
+		[bool]$DisableBehaviorMonitoring,
+		[bool]$DisableCatchupFullScan,
+		[bool]$DisableCatchupQuickScan,
+		[bool]$DisableEmailScanning,
+		[bool]$DisableIntrusionPreventionSystem,
+		[bool]$DisableIOAVProtection,
+		[bool]$DisablePrivacyMode,
+		[bool]$DisableRealtimeMonitoring,
+		[bool]$DisableRemovableDriveScanning,
+		[bool]$DisableRestorePoint,
+		[bool]$DisableScanningMappedNetworkDrivesForFullScan,
+		[bool]$DisableScanningNetworkFiles,
+		[bool]$DisableScriptScanning,
+		[string[]]$ExclusionExtension,
+	    [string[]]$ExclusionPath,
+		[string[]]$ExclusionProcess,	
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$HighThreatDefaultAction,
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$LowThreatDefaultAction,
+		[ValidateSet("Advanced","Basic","Disabled")]
+		[string]$MAPSReporting,
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$ModerateThreatDefaultAction,
+		[UInt32]$QuarantinePurgeItemsAfterDelay,
+		[bool]$RandomizeScheduleTaskTimes,
+		[ValidateSet("Both","Incoming","Outcoming")]
+		[string]$RealTimeScanDirection,
+		[ValidateSet("Everyday","Friday","Monday","Never","Saturday","Sunday","Thursday","Tuesday","Wednesday")]
+		[string]$RemediationScheduleDay,
+		[DateTime]$RemediationScheduleTime,
+		[UInt32]$ReportingAdditionalActionTimeOut,
+		[UInt32]$ReportingCriticalFailureTimeOut,
+		[UInt32]$ReportingNonCriticalTimeOut,
+		[UInt32]$ScanAvgCPULoadFactor,
+		[bool]$ScanOnlyIfIdleEnabled,
+		[ValidateSet("FullSCan","QuickScan")]
+		[string]$ScanParameters,
+		[UInt32]$ScanPurgeItemsAfterDelay,
+		[ValidateSet("Everyday","Friday","Monday","Never","Saturday","Sunday","Thursday","Tuesday","Wednesday")]
+		[string]$ScanScheduleDay,
+		[DateTime]$ScanScheduleQuickScanTime,
+		[DateTime]$ScanScheduleTime,
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$SevereThreatDefaultAction,
+		[UInt32]$SignatureAuGracePeriod,
+		[string]$SignatureDefinitionUpdateFileSharesSources,
+		[bool]$SignatureDisableUpdateOnStartupWithoutEngine,
+		[string]$SignatureFallbackOrder,
+		[UInt32]$SignatureFirstAuGracePeriod,
+		[ValidateSet("Everyday","Friday","Monday","Never","Saturday","Sunday","Thursday","Tuesday","Wednesday")]
+		[string]$SignatureScheduleDay,
+		[DateTime]$SignatureScheduleTime,
+		[UInt32]$SignatureUpdateCatchupInterval,
+		[UInt32]$SignatureUpdateInterval,
+		[ValidateSet("Always","Never","None")]
+		[string]$SubmitSamplesConsent,
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$ThreatIDDefaultAction_Actions,
+		[UInt64]$ThreatIDDefaultAction_Ids,
+		[bool]$UILockdown,
+		[ValidateSet("Allow","Block","Clean","NoAction","Quarantine","Remove","UserDefined")]
+		[string]$UnknownThreatDefaultAction
+
+    ) 
+
+   
+
+    Import-DscResource -ModuleName WindowsDefender -ModuleVersion "1.0.0.2"
+    Import-DscResource -ModuleName xActiveDirectory, xNetworking, xStorage, PSDesiredStateConfiguration
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+       LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+        ### Configure Antimalware ###
+        Service WindowsDefender
+		{
+			Name = 'WinDefend'
+			State = 'Running'
+			Ensure = 'Present'
+		}
+		WindowsDefender SetParameters
+		{
+			IsSingleInstance = $IsSingleInstance
+			ExclusionPath = $ExclusionPath
+			ExclusionExtension = $ExclusionExtension
+			ExclusionProcess = $ExclusionProcess
+			RealTimeScanDirection = $RealTimeScanDirection
+			RemediationScheduleDay = $RemediationScheduleDay	
+			ScanScheduleDay = $ScanScheduleDay
+			DisableRealtimeMonitoring = $DisableRealtimeMonitoring
+			DependsOn = "[Service]WindowsDefender"
+		}
+        ### Create AD PDC ###
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        Script script1
+        {
+      	    SetScript = {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+        xDisk ADDataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = 'F'
+            DependsOn = "[xWaitforDisk]Disk2"
+        }
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn="[xDisk]ADDataDisk"
+        }
+
+	WindowsFeature ADAdminCenter 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-AD-AdminCenter"
+			DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+		
+	WindowsFeature ADDSTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-ADDS-Tools"
+			DependsOn = "[WindowsFeature]ADDSInstall"
+        }  
+
+        xADDomain FirstDS
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        
+   }
+}
