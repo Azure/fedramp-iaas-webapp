98b4a523f18780ce75a2e00057dafbec98cf7de0
jomolesk <jomolesk@microsoft.com>
2018-04-29 19:58:04 -0400

    Resolve merge conflicts

--- predeploy/Orchestration_InitialSetup.ps1
+++ predeploy/Orchestration_InitialSetup.ps1
@@ -39,9 +39,6 @@ Must be the Domain name to be created. Validation exists in the script to ensure
 Write-Host "`n `nAzure Security and Compliance Blueprint - FedRAMP Web Applications Automation - Pre-Deployment Script `n" -foregroundcolor green
 Write-Host "This script can be used for creating the necessary preliminary resources to deploy a multi-tier web application architecture with pre-configured security controls to help customers achieve compliance with FedRAMP requirements. See https://aka.ms/fedrampblueprint for more information. `n " -foregroundcolor yellow
 
-Write-Host "`n DEFINE YOUR DOMAIN `n" -foregroundcolor green
-
-
 ########################################################################################################################
 # LOGIN TO AZURE FUNCTION
 ########################################################################################################################
@@ -51,7 +48,7 @@ function loginToAzure {
 		[int]$lginCount
 	)
 
-	Write-Host "Please login with your Azure Government credentials." -ForegroundColor Yellow
+	Write-Host "Please login using Azure Government credentials." -ForegroundColor Yellow
 	
 	Login-AzureRmAccount -EnvironmentName "AzureUSGovernment" -ErrorAction SilentlyContinue 	
 
@@ -65,7 +62,7 @@ function loginToAzure {
 			loginToAzure -lginCount $lginCount
 		} 
         else {
-			Write-Host "Your credentials are incorrect or invalid exceeding maximum retries. Make sure you are using your Azure Government account information." -ForegroundColor Magenta
+			Write-Host "Credentials input are incorrect, invalid, or exceed the maximum number of retries. Verify the Azure Government account information used is correct." -ForegroundColor Magenta
 			Write-Host "Press any key to exit..." -ForegroundColor Yellow
 			$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 			Exit
@@ -94,7 +91,7 @@ function checkKeyVaultName {
 # ADMIN USERNAME VALIDATION FUNCTION
 ########################################################################################################################
 function checkAdminUserName {
-    $username = Read-Host "Enter an admin username"
+    $username = Read-Host "Enter an Admin Username"
     if ($username.ToLower() -eq "admin") {
         Write-Host "Not a valid Admin username, please select another." -ForegroundColor Magenta  
         checkAdminUserName
@@ -107,25 +104,21 @@ function checkAdminUserName {
 # DOMAIN NAME VALIDATION FUNCTION
 ########################################################################################################################
 function CheckDomainName {  
-	[CmdletBinding()]
-	param(
-        [Parameter(Mandatory=$true)]
-	    [string]$domain
-    )
+    $domain = Read-Host "Domain Name"   
     if ($domain.length -gt "15") {
         Write-Host "Domain Name is too long. Must be less than 15 characters." -ForegroundColor Magenta 
         CheckDomainName
-        Return
+        return
     }
     if ($domain -notmatch "^[a-zA-Z0-9.-]*$") {
         Write-Host "Invalid character set utilized. Please verify domain name contains only alphanumeric, hyphens, and at least one period." -ForegroundColor Magenta 
         CheckDomainName
-        Return
+        return
     }
     if ($domain -notmatch "[.]") {
         Write-Host "Invalid Domain Name specified. Please verify domain name contains only alphanumeric, hyphens, and at least one period." -ForegroundColor Magenta  
         CheckDomainName
-        Return
+        return
     }
     Return $domain
 }
@@ -138,7 +131,7 @@ function checkPasswords {
 		[Parameter(Mandatory=$true)]
 		[string]$name
 	)
-	$password = Read-Host -assecurestring "Enter $($name)"
+	$password = Read-Host -assecurestring "Enter an $($name)"
     $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password)
     $pw2test = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
     [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
@@ -198,7 +191,7 @@ function checkPasswords {
         }
     } 
     else {
-        Write-Host "Password is not long enough - Passwords must be at least " + $passLength + " characters long." -ForegroundColor Magenta
+        Write-Host "Password is not long enough - Passwords must be at least $passLength characters long." -ForegroundColor Magenta
         checkPasswords -name $name
         return
     }
@@ -223,11 +216,6 @@ Function New-AlphaNumericPassword () {
     return $RandomPassword
 }
 
-Write-Host "Please provide a domain name you would like to use with this deployment." -ForegroundColor Yellow
-$DomainName = Read-Host "Domain Name" 
-Write-Host "`n"
-$domainused = checkdomainname $domainname
-
 function Generate-Cert() {
 	[CmdletBinding()]
 	param(
@@ -246,7 +234,7 @@ function Generate-Cert() {
 }
 
 ########################################################################################################################
-# Create KeyVault or setup existing keyVault
+# Create Key Vault
 ########################################################################################################################
 Write-Host "`n LOGIN TO AZURE `n" -foregroundcolor green
 
@@ -264,7 +252,7 @@ function orchestration {
 		[string]$adminUsername,
 		[Parameter(Mandatory=$true)]
 		[SecureString]$adminPassword,
-                [string]$domain = $domainused
+        [string]$domain
 	)
 
 	$errorActionPreference = 'stop'
@@ -318,7 +306,7 @@ function orchestration {
 			$aadClientID = $SvcPrincipals[0].ApplicationId;
 		}
 
-	# Create KeyVault or setup existing keyVault
+	# Create Key Vault
 	Write-Host "Creating resource group '$($resourceGroupName)' to hold key vault." -ForegroundColor Yellow
 	if (-not (Get-AzureRmResourceGroup -Name $resourceGroupName -Location $location -ErrorAction SilentlyContinue)) {
 		New-AzureRmResourceGroup -Name $resourceGroupName -Location $location  | Out-String | Write-Verbose
@@ -356,7 +344,7 @@ function orchestration {
 
 		$certPassword = New-AlphaNumericPassword
 		$secureCertPassword = ConvertTo-SecureString $certPassword -AsPlainText -Force
-		Generate-Cert -certPassword $secureCertPassword -domain $domain
+		Generate-Cert -certPassword $secureCertPassword -domain $domainused
 		$certificate = Get-Content -Path ".\cert.txt" | Out-String
 
         try {
@@ -418,22 +406,42 @@ function orchestration {
 
 try {
 	loginToAzure -lginCount 1
-	Write-Host "You will now be asked to create credentials for the administrator and sql service accounts. `n" -ForegroundColor Yellow
+	Write-Host "Creating credentials for the administrator accounts. `n" -ForegroundColor Yellow
 	Write-Host "`n CREATE CREDENTIALS `n" -foregroundcolor green
     $adminUsername = checkAdminUserName
-	$passwordNames = @("adminPassword")
+	$passwordNames = @("Admin Password")
 	$passwords = New-Object -TypeName PSObject
 	for ($i=0;$i -lt $passwordNames.Length;$i++) {
 	   checkPasswords -name $passwordNames[$i]
 	}
-	orchestration -adminUsername $adminUsername -adminPassword $passwords.adminPassword
+
+    # Set Domain Name   
+    Write-Host "`n DEFINE THE DOMAIN `n" -foregroundcolor green
+    Write-Host "Please provide a domain name for use with this deployment." -ForegroundColor Yellow
+    $domainused = checkdomainname 
+
+    #Set Resource Group and Azure SubscriptionID
+    Write-Host "`n DEFINE THE AZURE RESOURCES `n" -foregroundcolor green
+    Write-Host "Please provide the Azure Government Subscription ID for use with this deployment." -ForegroundColor Yellow
+    $AzureSubID = Read-Host "Azure Government Subscription ID"
+
+    Write-Host "`nPlease create a name for the Resource Group that will be used with this deployment." -ForegroundColor Yellow
+    $RGName = Read-Host "Resource Group Name"
+
+    $KVName = "$RGNAME-KV"
+	Write-Host "`nThe Key Vault resource for this deployment will be named:" -ForegroundColor Green
+	Write-Host "$KVName" -ForegroundColor Yellow
+	Write-Host "`n"
+
+    #Orchestration
+	orchestration -adminUsername $adminUsername -adminPassword $passwords.'Admin Password' -resourceGroupName $RGName -keyVaultName $KVName -subscriptionId $AzureSubID -domain $domainused
     Write-Host "`n ORCHESTRATION COMPLETE `n" -foregroundcolor green
     Write-Host "Initial Pre-Deployment and Orchestration operations for this blueprint template are complete. Please proceed with finishing the deployment through the portal link in the Quickstart section at https://aka.ms/fedrampblueprint." -foregroundcolor Yellow
 }
 
 catch {
 	Write-Host $PSItem.Exception.Message
-	Write-Host "An error has occurred in the pre-deployment orchestration setup. Please review any error messages before attempting a re-deployment. Thank You." -ForegroundColor Magenta
+	Write-Host "An error has occurred in the pre-deployment orchestration setup. Please review any error messages before attempting a re-deployment." -ForegroundColor Magenta
 	Write-Host "Press any key to exit..." -ForegroundColor Yellow
 	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 	Exit
